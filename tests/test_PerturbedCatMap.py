93c11d61947633087dbecdcc7dea394b523d4846
Nisha Chandramoorthy <nishac@mit.edu>
2020-02-25 16:53:54 -0500

    Plot directional derivatives of components of clvs.

--- tests/test_PerturbedCatMap.py
+++ tests/test_PerturbedCatMap.py
@@ -36,29 +36,36 @@ def test_daugmented_step():
 
 
 def plot_clvs():
-    fig, ax = subplots(1,1)
+    fig, ax = subplots(1,2)
     s = [0.7,0.3]
-    eps = 5.e-2
+    eps = 1.e-2
     d = 2
     u0 = random.rand(d,1)
-    n = 1000
+    n = 10000
     u = step(u0,s,n) #shape: (n+1)xdx1
     u = u[1:].T[0] # shape:dxn
     du = dstep(u,s) #shape:nxdxd
     P = clvs(u,du,d).T #shape:nxdxd
     v1 = P[0]
     v2 = P[1]
-    ax.plot(u[0], u[1], 'k.', ms=1)
-    ax.plot([u[0] - eps*v1[0], u[0] + eps*v1[0]],\
+    ax[0].plot([u[0] - eps*v1[0], u[0] + eps*v1[0]],\
             [u[1] - eps*v1[1], u[1] + eps*v1[1]],\
             lw=2.0, color='red')
-    ax.plot([u[0] - eps*v2[0], u[0] + eps*v2[0]],\
+    ax[1].plot([u[0] - eps*v2[0], u[0] + eps*v2[0]],\
             [u[1] - eps*v2[1], u[1] + eps*v2[1]],\
             lw=2.0, color='black')
+
+    ax[0].set_title('$V^1$',fontsize=24)
+    
+    ax[1].set_title('$V^2$',fontsize=24)
+    for j in range(2):
+            ax[j].xaxis.set_tick_params(labelsize=24)
+            ax[j].yaxis.set_tick_params(labelsize=24)
+
+
     return fig,ax
 
-#def compare_clvs():
-if __name__=="__main__":
+def plot_clv_components():
     fig, ax = subplots(2,2)
     s = [0.7,0.3]
     #s = zeros(2)
@@ -148,4 +155,184 @@ if __name__=="__main__":
             ax[i,j].yaxis.set_tick_params(labelsize=24)
 
 
-    #return fig,ax
+    return fig,ax
+
+def plot_clv_gradients():
+    s = [0.7,0.3]
+    #s = zeros(2)
+    eps = 4.e-2
+    d = 2
+    u0 = random.rand(d,1)
+    n = 20000
+    u = step(u0,s,n) #shape: (n+1)xdx1
+    u = u[1:].T[0] # shape:dxn
+    du = dstep(u,s) #shape:nxdxd
+    P = clvs(u,du,2).T #shape:nxdxd
+    v1 = P[0]
+    v2 = P[1]
+    n_grid = 500
+    x_x = linspace(0.,1.,n_grid)
+    x_grid, y_grid = meshgrid(x_x, x_x)
+    f = LinearNDInterpolator(u.T[10:],\
+            v2[1,10:],)
+    v2x = f(x_grid, y_grid).reshape(\
+            n_grid,n_grid)
+    dx = x_x[1]
+    #calculate x partial derivatives
+    dv2x_dx = (hstack([v2x[:,1:],\
+            v2x[:,0].reshape(-1,1)]) - \
+            hstack([v2x[:,-1].reshape(-1,1),\
+            v2x[:,0:-1]]))/(2*dx)
+    f = LinearNDInterpolator(u.T[10:],\
+            v1[1,10:],)
+    v1y = f(x_grid, y_grid).reshape(\
+            n_grid,n_grid)
+    dv1y_dx = (hstack([v1y[:,1:],\
+            v1y[:,0].reshape(-1,1)]) - \
+            hstack([v1y[:,-1].reshape(-1,1),\
+            v1y[:,0:-1]]))/(2*dx)
+    #calculate y partial derivatives
+    dv2x_dy = (vstack([v2x[1:,:],\
+            v2x[0,:].reshape(1,-1)]) - \
+            vstack([v2x[-1,:].reshape(1,-1),\
+            v2x[0:-1,:]]))/(2*dx)
+    dv1y_dy = (vstack([v1y[1:,:],\
+                    v1y[0,:].reshape(1,-1)]) - \
+                    vstack([v1y[-1,:].reshape(1,-1),\
+                    v1y[0:-1,:]]))/(2*dx)
+
+    fig, ax = subplots(2,2)
+    cntr00 = ax[0,0].contourf(x_grid,\
+            y_grid, dv1y_dx, levels=linspace(nanmin(dv1y_dx),\
+            nanmax(dv1y_dx)/1.5,30))
+    cntr01 = ax[0,1].contourf(x_grid,\
+            y_grid, dv2x_dx,levels=linspace(nanmin(dv2x_dx)/5,\
+            nanmax(dv2x_dx)/8,30))
+    cntr10 = ax[1,0].contourf(x_grid,\
+            y_grid, dv1y_dy,levels=linspace(nanmin(dv1y_dy),nanmax(dv1y_dy),30))
+    cntr11 = ax[1,1].contourf(x_grid,\
+            y_grid, dv2x_dy,levels=linspace(nanmin(dv2x_dy)/5,nanmax(dv2x_dy)/5,30))
+
+
+    fig.colorbar(cntr00, ax=ax[0,0])
+    ax[0,0].set(xlim=(0, 1), ylim=(0, 1))
+    fig.colorbar(cntr01, ax=ax[0,1])
+    ax[0,1].set(xlim=(0, 1), ylim=(0, 1))
+    fig.colorbar(cntr10, ax=ax[1,0])
+    ax[1,0].set(xlim=(0, 1), ylim=(0, 1))
+    fig.colorbar(cntr11, ax=ax[1,1])
+    ax[1,1].set(xlim=(0, 1), ylim=(0, 1))
+
+    for i in range(2):
+        for j in range(2):
+            ax[i,j].xaxis.set_tick_params(labelsize=24)
+            ax[i,j].yaxis.set_tick_params(labelsize=24)
+
+
+    ax[0,0].set_title('$\partial_1 V^1_2$',fontsize=24)
+
+    ax[0,1].set_title('$\partial_1 V^2_1$',fontsize=24)
+
+    ax[1,0].set_title('$\partial_2 V^1_2$',fontsize=24)
+
+    ax[1,1].set_title('$\partial_2 V^2_1$',fontsize=24)
+    return fig, ax
+
+if __name__=="__main__":
+#def plot_clv_directional_derivatives():
+    s = [0.7,0.3]
+    #s = zeros(2)
+    eps = 4.e-2
+    d = 2
+    u0 = random.rand(d,1)
+    n = 30000
+    u = step(u0,s,n) #shape: (n+1)xdx1
+    u = u[1:].T[0] # shape:dxn
+    du = dstep(u,s) #shape:nxdxd
+    P = clvs(u,du,2).T #shape:nxdxd
+    v1 = P[0]
+    v2 = P[1]
+    n_grid = 100
+    x_x = linspace(0.,1.,n_grid)
+    x_grid, y_grid = meshgrid(x_x, x_x)
+    f = LinearNDInterpolator(u.T[10:],\
+            v1[0,10:],)
+    v1x = f(x_grid, y_grid).reshape(\
+            n_grid,n_grid)
+    f = LinearNDInterpolator(u.T[10:],\
+            v1[1,10:],)
+    v1y = f(x_grid, y_grid).reshape(\
+            n_grid,n_grid)
+    f = LinearNDInterpolator(u.T[10:],\
+            v2[0,10:],)
+    v2x = f(x_grid, y_grid).reshape(\
+            n_grid,n_grid)
+    f = LinearNDInterpolator(u.T[10:],\
+            v2[1,10:],)
+    v2y = f(x_grid, y_grid).reshape(\
+            n_grid,n_grid)
+
+    dx = x_x[1]
+    #calculate x partial derivatives
+    dv1y_dx = (hstack([v1x[:,1:],\
+            v1x[:,0].reshape(-1,1)]) - \
+            hstack([v1x[:,-1].reshape(-1,1),\
+            v1x[:,0:-1]]))/(2*dx)
+    dv2y_dx = (hstack([v2x[:,1:],\
+            v2x[:,0].reshape(-1,1)]) - \
+            hstack([v2x[:,-1].reshape(-1,1),\
+            v2x[:,0:-1]]))/(2*dx)
+    #calculate y partial derivatives
+    dv1y_dy = (vstack([v1x[1:,:],\
+            v1x[0,:].reshape(1,-1)]) - \
+            vstack([v1x[-1,:].reshape(1,-1),\
+            v1x[0:-1,:]]))/(2*dx)
+    dv2y_dy = (vstack([v2x[1:,:],\
+                    v2x[0,:].reshape(1,-1)]) - \
+                    vstack([v2x[-1,:].reshape(1,-1),\
+                    v2x[0:-1,:]]))/(2*dx)
+
+    # calculate directional derivatives
+    dv1y_dv1 = dv1y_dx*v1x + dv1y_dy*v1y
+    dv1y_dv2 = dv1y_dx*v2x + dv1y_dy*v2y
+    dv2y_dv1 = dv2y_dx*v1x + dv2y_dy*v1y
+    dv2y_dv2 = dv2y_dx*v2x + dv2y_dy*v2y
+
+
+    fig, ax = subplots(2,2)
+    cntr00 = ax[0,0].contourf(x_grid,\
+            y_grid, dv1y_dv1, levels=linspace(nanmin(dv1y_dv1),\
+            nanmax(dv1y_dv1),50))
+    cntr01 = ax[0,1].contourf(x_grid,\
+            y_grid, dv1y_dv2,levels=linspace(nanmin(dv1y_dv2),\
+            nanmax(dv1y_dv2),50))
+    cntr10 = ax[1,0].contourf(x_grid,\
+            y_grid, dv2y_dv1,levels=linspace(nanmin(dv2y_dv1),nanmax(dv2y_dv1),50))
+    cntr11 = ax[1,1].contourf(x_grid,\
+            y_grid, dv2y_dv2,levels=linspace(nanmin(dv2y_dv1),nanmax(dv2y_dv2),50))
+
+
+    fig.colorbar(cntr00, ax=ax[0,0])
+    ax[0,0].set(xlim=(0, 1), ylim=(0, 1))
+    fig.colorbar(cntr01, ax=ax[0,1])
+    ax[0,1].set(xlim=(0, 1), ylim=(0, 1))
+    fig.colorbar(cntr10, ax=ax[1,0])
+    ax[1,0].set(xlim=(0, 1), ylim=(0, 1))
+    fig.colorbar(cntr11, ax=ax[1,1])
+    ax[1,1].set(xlim=(0, 1), ylim=(0, 1))
+
+    for i in range(2):
+        for j in range(2):
+            ax[i,j].xaxis.set_tick_params(labelsize=24)
+            ax[i,j].yaxis.set_tick_params(labelsize=24)
+
+
+    ax[0,0].set_title('$D V^1_2\cdot V^1$',fontsize=24)
+
+    ax[0,1].set_title('$D V^1_2\cdot V^2$',fontsize=24)
+
+    ax[1,0].set_title('$D V^2_2\cdot V^1$',fontsize=24)
+
+    ax[1,1].set_title('$D V^2_2 \cdot V^2$',fontsize=24)
+    #return fig, ax
+
