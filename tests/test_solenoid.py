8ff1caeb83ff43b217ea83ff9e5e4c64cfe03f7e
Nisha Chandramoorthy <nishac@mit.edu>
2020-05-19 16:28:01 -0400

    Add test for clvs.

--- tests/test_solenoid.py
+++ tests/test_solenoid.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.insert(0,'../examples/')
+sys.path.insert(0,'../src/')
 from solenoid import *
+from clvs import *
 from numpy import *
 from scipy.interpolate import *
 from mpl_toolkits.mplot3d import Axes3D
@@ -40,3 +42,38 @@ def test_dstep():
     assert(allclose(du_dx, du_dx_fd))
     assert(allclose(du_dy, du_dy_fd))
     assert(allclose(du_dz, du_dz_fd))
+def test_clv():
+#if __name__=="__main__":
+    s = [1.,1.e5]
+    u = rand(3,1)
+    n = 1000
+    u_trj = step(u,s,n).T[0]
+    d, n = u_trj.shape
+    d_u = 3
+    du_trj = dstep(u_trj, s)
+    clv_trj = clvs(u_trj, du_trj, d_u)
+    n_spinup = 100
+    v1 = clv_trj[n_spinup:-n_spinup,:,0].T
+    v2 = clv_trj[n_spinup:-n_spinup,:,1].T
+    v3 = clv_trj[n_spinup:-n_spinup,:,2].T
+    u_trj = u_trj[:,n_spinup:-n_spinup]
+    n = n - 2*n_spinup
+    x, y, z = u_trj[0], u_trj[1], u_trj[2]
+    abs_r = sqrt(x*x + y*y)
+    theta = reshape([-y/abs_r, x/abs_r],[2, -1]).T
+    z_dir = reshape([zeros(n), zeros(n), ones(n)],[3,-1]).T
+    r_dir = reshape([x/abs_r, y/abs_r, z],[3,-1]).T
+    assert(allclose(diag(dot(theta, v2[0:2])),\
+            zeros(n)))
+    assert(allclose(diag(dot(theta, v3[0:2])),\
+            zeros(n)))
+    #assert(allclose(diag(dot(z_dir, v2)),\
+     #       zeros(n)))
+    #assert(allclose(diag(dot(r_dir, v1)),\
+     #       zeros(n)))
+   
+    #assert(allclose(v1[2],zeros(n)))
+    #assert(allclose(v1[2],zeros(n)))
+       
+    #assert(allclose(v1[2],zeros(n)))
+
